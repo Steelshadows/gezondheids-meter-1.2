1b63f0f79acf099e3cb7d570afc605442287971d
Niels <niels.kramer24@gmail.com>
2021-01-06 23:04:02 +0100

    Testlayout added

--- dashboard.php
+++ dashboard.php
@@ -3,16 +3,6 @@ include "parts/header.php";
 require_once('php/function/user.inc.php');
 ?>
 
-<main>
-    <h1>Gezondheidstest</h1>
-    <p>Als je vitaal wilt blijven leven is het belangrijk dat je gezond leeft. Gezond leven bestaat uit voldoende fruit
-        en groente eten, genoegen beweging en rustig aandoen met roken en alchohol.<br><br>
-
-        Wil je weten hoe jij het er in het dagelijks leven vanaf brengt? Doe dan de gezondheidstest!</p><br><br>
-
-    <a class="test-btn" href="test.php">Doe de test!</a>
-</main>
-
 
 
 
