ab3b2b430497f549b41709b2974b6467c7715403
Steelshadows <its.a.me.brand@gmail.com>
2021-01-22 11:03:06 +0100

    navbar edit

--- includes/html/navbar.php
+++ includes/html/navbar.php
@@ -6,9 +6,9 @@
     <h1>Gezondheidsmeter</h1>
     <div class="navbar-content <?=$userLoggedIn !== true ? 'justify-content-center' : 'justify-content-end'?>">
         <?php if($userLoggedIn) { ?>
-            <a href="profile.php">ingelogd als: <span><?=$_SESSION['user']['username']?></span></a>
-            <a class="nav-link">dashboard</a>
-            <a class="nav-link" href="logout.php">uitloggen</a>
+            <a class="w33" href="profile.php">ingelogd als: <span><?=$_SESSION['user']['username']?></span></a>
+            <a class="nav-link w33" href="dashboard.php">dashboard</a>
+            <a class="nav-link w33" href="logout.php">uitloggen</a>
         <?php } else { ?>
             <a href="index.php" class="nav-link">inloggen</a>
             <a href="register.php" class="nav-link">registeren</a>
